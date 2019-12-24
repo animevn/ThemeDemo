3037ac93356726ca297a481a7e0e251d702033fd
animevn <haanhth21@gmail.com>
2019-12-24 20:39:26 +0700

    add night mode to options menu

--- app/src/main/java/com/haanhgs/app/themedemo/MainActivity.java
+++ app/src/main/java/com/haanhgs/app/themedemo/MainActivity.java
@@ -1,12 +1,15 @@
 package com.haanhgs.app.themedemo;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -87,5 +90,37 @@ public class MainActivity extends AppCompatActivity {
                 tvScore2.setText(String.format("%s", score2));
                 break;
         }
+    }
+
+    //change theme
+    private void changeIconByTheme(Menu menu){
+        int dark = AppCompatDelegate.getDefaultNightMode();
+        if (dark == AppCompatDelegate.MODE_NIGHT_YES){
+            menu.findItem(R.id.mniTheme).setIcon(R.drawable.dark);
+        }else {
+            menu.findItem(R.id.mniTheme).setIcon(R.drawable.light);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        changeIconByTheme(menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        int id = item.getItemId();
+        //to change to darkmode , need to change style to this line in styles.xml
+        //<style name="AppTheme" parent="Theme.AppCompat.DayNight.DarkActionBar">
+        int dark = AppCompatDelegate.getDefaultNightMode();
+        if (dark == AppCompatDelegate.MODE_NIGHT_YES){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+//        recreate();
+        return super.onOptionsItemSelected(item);
     }
 }
